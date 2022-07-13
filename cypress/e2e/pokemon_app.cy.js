3fb970e26e627247f3cbf8fb44fb76981eb4a16c
MarcoZeee <MarcoZeee@github.com>
2022-07-13 13:01:03 +0800

    ci: fs-11.9

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -3,6 +3,11 @@ describe('Pokedex', function() {
       cy.visit('http://localhost:5000')
       cy.contains('ivysaur')
       cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
-  })
+    }),
+    it('certain pokemon can be opened', function() {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur').click()
+      cy.contains('speed')
+    });
+  });
 
