9efaece3b8d5c6df5c5017d70b9a494e08c2a290
MarcoZeee <MarcoZeee@github.com>
2022-07-13 23:51:57 +0800

    ci: optimization

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -1,13 +1,17 @@
-describe('Pokedex', function() {
-    it('front page can be opened', function() {
-      cy.visit('http://localhost:5000')
-      cy.contains('ivysaur')
-      cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    }),
-    it('certain pokemon can be opened', function() {
-      cy.visit('http://localhost:5000')
-      cy.contains('ivysaur').click()
-      cy.contains('speed')
-    });
+describe("Pokedex", function () {
+  beforeEach(function () {
+    cy.visit("http://localhost:5000/");
+    cy.wait(2000);
   });
-
+  it("front page can be opened", function () {
+    cy.contains("ivysaur");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+  it("certain pokemon can be opened", function () {
+    cy.contains("ivysaur").click();
+    cy.wait(2000);
+    cy.contains("speed");
+  });
+});
